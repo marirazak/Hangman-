558da5150ea83a84cbae6c93d50694c1a5846044
Maya S Fall <45639325+sterlingF@users.noreply.github.com>
2020-06-02 07:53:07 -0700

    Update guess.hpp

--- guess.hpp
+++ guess.hpp
@@ -1,18 +1,19 @@
+
 #ifndef __GUESS_HPP__
 #define __GUESS_HPP__
 #include <string>
 #include <iostream>
 #include <cstring>
-
+#include <vector>
 class Guess{
 public:
     std::string place;
     std::string ourguess;
     Guess(std::string word, std::string guess): place(word), ourguess(guess) {};
+    std::vector<Guess*> guesslist; 
     
     
-    //checks if guess is correct, true if yes, false if no    
-    bool guess_correct(){
+bool guess_correct(){
         for(int i = 0; i < place.size(); i++){
             char* b = new char(ourguess.length()+1);
             std::strcpy(b, ourguess.c_str());
@@ -23,6 +24,21 @@ public:
         
         return false;
     }
-    
+
+void add(std::vector<Guess*> v){
+	v.push_back(this); 
+	guesslist = v; 
+}
+ 
+void printguess(){
+	for(int i = 0; i < guesslist.size(); i++){
+		std::cout << guesslist.at(i)->ourguess;
+	}
+}   
+std::vector<Guess*> returnvec(){
+	return guesslist; 
+
+}
+
 };
 #endif
